bb0dd39411b0b5b4c1eb4c4cdb4b5e9775cf4118
Andrew Simpson <asimpson1989@googlemail.com>
2022-10-14 17:58:37 +0100

    feat: added len() to metrics to count numbers of posts

--- techtrends/app.py
+++ techtrends/app.py
@@ -3,6 +3,13 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+# Test Code #
+db_connection_count = 0
+db_connection_count += 1
+# End of Test Code #
+
+
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
@@ -45,8 +52,11 @@ def healthcheck():
 # Define the metrics route of the web application 
 @app.route('/metrics')
 def metrics():
+    connection = sqlite3.connect('database.db')
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    post_count = len(posts)
     response = app.response_class(
-            response=json.dumps({"db_connection_count": 1, "post_count": 7}),
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": post_count}),
             status=200,
             mimetype='application/json'
     )
