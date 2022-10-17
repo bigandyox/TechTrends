b1cc16347a3155a7e65ec79c18ee80bf4d6e5d97
Andrew Simpson <asimpson1989@googlemail.com>
2022-10-17 12:24:19 +0100

    fix: removed logging features from main

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,6 +1,4 @@
 import sqlite3
-import logging
-import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
@@ -100,6 +98,4 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   # Stream logs to a file
-   logging.basicConfig(filename='app.log',level=logging.DEBUG)
    app.run(host='0.0.0.0', port='3111')
